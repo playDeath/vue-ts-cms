360221404d32d3911564f36d3fc4011d59ae15f3
playDeath <863834859@qq.com>
2021-08-11 21:29:00 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -95,18 +95,21 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
-    msg: String,
-  },
-});
+
+    msg: String
+  }
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="less">
+<style scoped
+
+ lang="less">
 h3 {
   margin: 40px 0 0;
 }
